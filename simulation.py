063a37ab6679205b8570c42cbeb45d694482ad74
rofdo <git.hub@ewe.net>
2022-10-12 11:14:04 +0200

    fixed small bug

--- simulation.py
+++ simulation.py
@@ -1,17 +1,17 @@
-import copy
 import random
 from PIL import Image
 
-SIZE_X = 500
-SIZE_Y = 500
+SIZE_X = 200
+SIZE_Y = 200
+
 
 class Crystal:
     def __init__(self):
-        resolution = 100
-        self.up_time = random.randint(1, resolution)
-        self.right_time = random.randint(1, resolution)
-        self.down_time = random.randint(1, resolution)
-        self.left_time = random.randint(1, resolution)
+        resolution = 60
+        self.up_time = random.randint(50, resolution)
+        self.right_time = random.randint(50, resolution)
+        self.down_time = random.randint(50, resolution)
+        self.left_time = random.randint(50, resolution)
 
     def get_value(self, direction):
         if direction == "up":
@@ -31,6 +31,10 @@ def clamp(value, low, high):
         return low
     else:
         return value
+
+
+def is_clamp(value, low, high):
+    return value > high or value < low
 
 
 def eval(x):
@@ -57,7 +61,7 @@ def sort_evaluator(e):
     return e["time"]
 
 
-def calculate_growth_order(crystals):
+def calculate_growth_order(crystals) -> []:
     order = []
     for crystal, i in zip(crystals, range(len(crystals))):
         order.append({"time": crystal.get_value("up"), "direction": "up", "crystal_index": i})
@@ -70,43 +74,13 @@ def calculate_growth_order(crystals):
 
 def sprinkle_cristals(crystal_map, amount):
     new_map = crystal_map
-    size_x = len(crystal_map) - 1
-    size_y = len(crystal_map[0]) - 1
+    size_x = SIZE_X - 1
+    size_y = SIZE_Y - 1
     for i in range(amount):
         x, y = random.randint(0, size_x), random.randint(0, size_y)
         while new_map[x][y] != -1:
             x, y = random.randint(0, size_x), random.randint(0, size_y)
         new_map[x][y] = i
-    return new_map
-
-
-def grow(index, direction, crystal_map: []):
-    # new_map = copy.deepcopy(crystal_map)
-    # new_map = crystal_map
-    new_map = []
-    for x in range(len(crystal_map)):
-        new_map.append([])
-        for y in range(len(crystal_map[0])):
-            new_map[x].append(crystal_map[x][y])
-
-    offset_x, offset_y = 0, 0
-    if direction == "up":
-        offset_x, offset_y = 0, 1
-    elif direction == "right":
-        offset_x, offset_y = 1, 0
-    elif direction == "down":
-        offset_x, offset_y = 0, -1
-    elif direction == "left":
-        offset_x, offset_y = -1, 0
-
-    size_x = len(crystal_map)
-    size_y = len(crystal_map[0])
-    for row, x in zip(crystal_map, range(size_x)):
-        for item, y in zip(row, range(size_y)):
-            if item == index:
-                check_x, check_y = clamp(x + offset_x, 0, size_x - 1), clamp(y + offset_y, 0, size_y - 1)
-                if crystal_map[check_x][check_y] == -1:
-                    new_map[check_x][check_y] = index
     return new_map
 
 
@@ -128,16 +102,17 @@ def quick_grow(index, direction, crystal_map):
                 growing_crystals.append({"x": x, "y": y})
 
     for crystal in growing_crystals:
-        goal_field_x = clamp(crystal["x"] + offset_x, 0, SIZE_X - 1)
-        goal_field_y = clamp(crystal["y"] + offset_y, 0, SIZE_Y - 1)
-        if crystal_map[goal_field_x][goal_field_y] == -1:
-            crystal_map[goal_field_x][goal_field_y] = index
+        goal_field_x = crystal["x"] + offset_x
+        goal_field_y = crystal["y"] + offset_y
+        if not (is_clamp(goal_field_x, 0, SIZE_X - 1) or is_clamp(goal_field_y, 0, SIZE_Y - 1)):
+            if crystal_map[goal_field_x][goal_field_y] == -1:
+                crystal_map[goal_field_x][goal_field_y] = index
 
     return crystal_map
 
 
-def simulate(size_x=500, size_y=500, crystal_amount=50, iterations=1000):
-    crystal_map = mk_matrix(size_x, size_y, -1)
+def simulate(crystal_amount=50, iterations=1000):
+    crystal_map = mk_matrix(SIZE_X, SIZE_Y, -1)
 
     crystals = mk_crystals(crystal_amount)
     growth_order = calculate_growth_order(crystals)
@@ -146,33 +121,31 @@ def simulate(size_x=500, size_y=500, crystal_amount=50, iterations=1000):
 
     for i in range(iterations):
         # Wachstumsaktion durchführen
-        for action in growth_order:
-            if action["time"] == growth_order[0]["time"]:
-                crystal_map = quick_grow(action["crystal_index"], action["direction"], crystal_map)
+        crystal_map = quick_grow(growth_order[0]["crystal_index"], growth_order[0]["direction"], crystal_map)
 
-                # Zeit aktueller Aktion von allen anderen abziehen
-                for e in range(len(growth_order)):
-                    growth_order[e]["time"] = growth_order[e]["time"] - action["time"]
+        # Zeit aktueller Aktion von allen anderen abziehen
+        length = len(growth_order) - 1
+        for e in range(length):
+            growth_order[length - e]["time"] -= growth_order[0]["time"]
 
-                # Zeit für ausgeführte Aktion zurücksetzten
-                action["time"] = crystals[action["crystal_index"]].get_value(action["direction"])
-                print(action["time"])
+        # Zeit für ausgeführte Aktion zurücksetzten
+        growth_order[0]["time"] = crystals[growth_order[0]["crystal_index"]].get_value(growth_order[0]["direction"])
 
         # Sortieren
-        # growth_order = sorted(growth_order, key=lambda x: x["time"])
-        growth_order.sort(key=eval)
+        growth_order = sorted(growth_order, key=lambda x: x["time"])
+        # growth_order.sort(key=eval)
 
         print(str(round((i / iterations) * 100 * 10) / 10) + "%")
 
     # Bild erstellen
-    img = Image.new('RGB', (size_x, size_y), "black")
-    pixels = img.load()
-    for f in range(img.size[0]):
-        for j in range(img.size[1]):
-            v = clamp(crystal_map[f - 1][j - 1] * 5, 0, 255)
-            pixels[f, j] = (v, v, v)
-    img.save("test" + str(0) + ".png")
+        img = Image.new('RGB', (SIZE_X, SIZE_Y), "black")
+        pixels = img.load()
+        for f in range(img.size[0]):
+            for j in range(img.size[1]):
+                v = clamp(crystal_map[f - 1][j - 1] * 5, 0, 255)
+                pixels[f - 1, j - 1] = (v, v, v)
+        img.save("test" + str(i) + ".png")
 
 
 if __name__ == "__main__":
-    simulate(size_x=500, size_y=500, crystal_amount=50, iterations=1000)
+    simulate(crystal_amount=50, iterations=10000)
