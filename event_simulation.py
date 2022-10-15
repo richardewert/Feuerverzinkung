09e3b3f3cbb7574b0a8a1f07e00127d267074c2f
rofdo <git.hub@ewe.net>
2022-10-15 16:48:49 +0200

    refactoring

--- event_simulation.py
+++ event_simulation.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import random
 from tqdm import tqdm
@@ -6,11 +7,11 @@ import numpy as np
 from PIL import Image
 from numpy import array
 
-events = []
-size_x = 500
-size_y = 500
+events = collections.deque()
+size_x = 1920
+size_y = 1080
 crystal_image = np.zeros((size_y, size_x))
-simulated_pixels = 10
+simulated_pixels = 0
 
 
 def init_events(amount=50, min_time=50, max_time=100):
@@ -35,7 +36,10 @@ def init_events(amount=50, min_time=50, max_time=100):
 
 def render(crystals, name):
     im = array(crystals)
-    Image.fromarray(im).show()
+    im = Image.fromarray(im)
+    im = im.convert("L")
+    im.show()
+    im.save(str(name) + ".png")
 
 
 class GrowthEvent:
@@ -59,51 +63,19 @@ def fire(event):
             [event.position[0] + -1, event.position[1] + 0]
         ]
         for position, direction in zip(positions, range(4)):
-            if size_x > position[0] >= 0 and size_y > position[1] >= 0:
+            if size_y > position[0] >= 0 and size_x > position[1] >= 0:
                 if crystal_image[position[0]][position[1]] == 0:
-                    object = GrowthEvent(event.time_offsets, event.time, direction, position, event.color)
-                    done = False
-                    j = 0
-                    time = event.time + event.time_offsets[direction]
-                    while not done:
-                        if j < len(events):
-                            if events[j].time > time:
-                                events.insert(j, object)
-                                done = True
-                            if events[j].position == position:
-                                done = True
-                            j += 1
-                        else:
-                            events.append(object)
-                            done = True
-
-
-def fire_sort(event):
-    if crystal_image[event.position[0]][event.position[1]] == 0:
-        crystal_image[event.position[0]][event.position[1]] = event.color
-        global simulated_pixels
-        simulated_pixels += 1
-
-        positions = [
-            [event.position[0] + 0, event.position[1] + 1],
-            [event.position[0] + 0, event.position[1] + -1],
-            [event.position[0] + 1, event.position[1] + 0],
-            [event.position[0] + -1, event.position[1] + 0]
-        ]
-        for position, direction in zip(positions, range(4)):
-            if size_x > position[0] >= 0 and size_y > position[1] >= 0:
-                if crystal_image[position[0]][position[1]] == 0:
-                    object = GrowthEvent(event.time_offsets, event.time, direction, position, event.color)
-                    events.append(object)
+                    new_event = GrowthEvent(event.time_offsets, event.time, direction, position, event.color)
+                    events.append(new_event)
         events.sort(key=lambda x: x.time)
 
 
 if __name__ == "__main__":
-    events = init_events()
+    events = init_events(amount=round(size_x*size_y/1000))
     for i in tqdm(range(size_x * size_y)):
         old_simulated = simulated_pixels
         while simulated_pixels <= old_simulated:
-            fire(events.pop(0))
+            fire_sort(events.pop(0))
             logging.debug(crystal_image)
 
     render(crystal_image, "result")
